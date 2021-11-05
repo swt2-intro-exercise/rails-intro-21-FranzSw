d9cd50ec95d5cee4b327ef2d904e61099ae48c92
Franz Sauerwald <15076254+FranzSw@users.noreply.github.com>
2021-11-05 11:51:12 +0100

    test: add test for author model

---
+++ spec/models/author_spec.rb
@@ -0,0 +1,10 @@
+
+
+describe "Authors model", type: :model do
+  it "should have first name, last name and homepage" do
+    author = build :author
+    expect(author.first_name).to eq("Alan")
+    expect(author.last_name).to eq("Turing")
+    expect(author.homepage).to eq("http://wikipedia.de/Alan_Turing")
+  end
+end
