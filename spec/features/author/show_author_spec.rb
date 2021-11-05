c00df3b3bf8ee44b04687d8b39674d441e8fe762
Franz Sauerwald <15076254+FranzSw@users.noreply.github.com>
2021-11-05 14:04:00 +0100

    test: add test for show author page

---
+++ spec/features/author/show_author_spec.rb
@@ -0,0 +1,10 @@
+
+describe "Show author page" do
+  it "should exist and render without error" do
+    @alan = create :author
+    visit author_path(@alan)
+    expect(page).to have_text(@alan.first_name)
+    expect(page).to have_text(@alan.last_name)
+    expect(page).to have_text(@alan.homepage)
+  end
+end
