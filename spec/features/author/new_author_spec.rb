2eb06083fbe3a59cb02f82a8cc3ab25dba624f49
Franz Sauerwald <15076254+FranzSw@users.noreply.github.com>
2021-11-05 11:33:26 +0100

    test: add tests for new authors form

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -5,4 +5,22 @@ describe "New author page", type: :feature do
     # https://guides.rubyonrails.org/routing.html#path-and-url-helpers
     visit new_author_path
   end
+
+  describe "form", type: :feature do
+    before(:each) do
+      visit new_author_path
+    end
+
+    it "should have a text input for author's first name" do
+      expect(page).to have_field("author[first_name]")
+    end
+  
+    it "should have a text input for author's last name" do
+      expect(page).to have_field("author[last_name]")
+    end
+  
+    it "should have a text input for author's home page" do
+      expect(page).to have_field("author[homepage]")
+    end
+  end
 end
