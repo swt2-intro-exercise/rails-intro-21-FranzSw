953a24ccab2f2bffea8502d033d7089b1ce6d39f
Franz Sauerwald <15076254+FranzSw@users.noreply.github.com>
2021-11-05 14:50:17 +0100

    feat: add author validation

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,7 @@
 class Author < ApplicationRecord
+  validates :last_name, presence: true, length: { minimum: 2 }
+  validates :first_name, presence: true
+
   def name
     return "#{first_name} #{last_name}"
   end
