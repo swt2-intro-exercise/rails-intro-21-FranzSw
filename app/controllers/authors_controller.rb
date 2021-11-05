d8675a4f5a131fcfe6642d3f5555dee7055025a2
Franz Sauerwald <15076254+FranzSw@users.noreply.github.com>
2021-11-05 13:49:00 +0100

    feat: save author on form submission

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -4,4 +4,16 @@ class AuthorsController < ApplicationController
 
   def new
   end
+
+  def create
+    @author = Author.new(author_params)
+    @author.save
+
+    redirect_to root_path, notice: "Author #{@author.name} saved!"
+  end
+  
+  private
+    def author_params
+      params.require(:author).permit(:first_name, :last_name, :homepage)
+    end
 end
