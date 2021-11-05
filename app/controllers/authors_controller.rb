9405310fd73a83a2ab90f1aae0a2690d618b64fb
Franz Sauerwald <15076254+FranzSw@users.noreply.github.com>
2021-11-05 23:12:23 +0100

    feat: add link to delete author

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -29,6 +29,13 @@ class AuthorsController < ApplicationController
     @author = Author.find(params[:id])
     @author.update(author_params)
   end
+
+  def destroy
+    @author = Author.find(params[:id])
+    @author.destroy
+
+    redirect_to authors_path
+  end
   
   private
     def author_params
