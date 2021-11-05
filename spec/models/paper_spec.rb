809a47ce49f8bac16d2d5d1eb130335bccd4897e
Franz Sauerwald <15076254+FranzSw@users.noreply.github.com>
2021-11-05 23:58:25 +0100

    test: add test for year and venue validation

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -6,4 +6,23 @@ RSpec.describe Paper, type: :model do
     paper.title = nil
     expect(paper).to_not be_valid
   end
+
+  it "should not be valid without venue" do
+    paper = build :paper
+    paper.venue = nil
+    expect(paper).to_not be_valid
+  end
+
+  it "should not be valid without (numeric) year" do
+    paper = build :paper
+    paper.year = nil
+    expect(paper).to_not be_valid
+    paper.year = "test"
+    expect(paper).to_not be_valid
+  end
+
+  it "should be valid if title, venue and year are given" do
+    paper = build :paper
+    expect(paper).to be_valid
+  end
 end
