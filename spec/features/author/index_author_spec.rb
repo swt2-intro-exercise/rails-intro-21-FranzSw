ef367f7e7e93db474814f21afb4c5ec3a985f8cc
Franz Sauerwald <15076254+FranzSw@users.noreply.github.com>
2021-11-05 23:12:10 +0100

    test: add test for delete author

--- spec/features/author/index_author_spec.rb
+++ spec/features/author/index_author_spec.rb
@@ -10,4 +10,11 @@ describe "Index authors page", type: :feature do
     expect(page).to have_link nil, href: author_path(alan)
     expect(page).to have_link nil, href: edit_author_path(alan)
   end
+
+  it "should delete author on click" do
+    alan = create :author
+    visit authors_path
+    expect(page).to have_link "Delete", href: author_path(alan)
+    find("a[data-method='delete'][href='#{author_path(alan)}']").click
+  end
 end
