4cb0b26d6caf8d0e915ae7a8ff255ed0ebae1654
Franz Sauerwald <15076254+FranzSw@users.noreply.github.com>
2021-11-06 12:01:06 +0100

    test: add update author test

--- spec/features/papers/edit.html.erb_spec.rb
+++ spec/features/papers/edit.html.erb_spec.rb
@@ -10,4 +10,21 @@ RSpec.describe "papers/edit", type: :feature do
     expect(page).to have_field("paper[title]")
     expect(page).to have_field("paper[author_ids][]")
   end
+
+  it "should save updated authors" do
+    # Ensure that atleast one, unique author is shown
+    @author = build :author
+    @author.first_name = "EditPaperUpdateAuthorTest"
+    @author.save
+
+    @paper = create :paper
+    visit edit_paper_path(@paper)
+    
+    # Select the author
+    select @author.name, :from => "paper[author_ids][]"
+    find('input[type="submit"]').click
+
+    @paper.reload
+    expect(@paper.authors).to include(@author)
+  end
 end
