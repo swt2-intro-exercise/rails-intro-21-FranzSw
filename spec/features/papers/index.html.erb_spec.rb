dc9ae406b694d409ad9614f2c4ee182c19074024
Franz Sauerwald <15076254+FranzSw@users.noreply.github.com>
2021-11-06 12:23:56 +0100

    test: add test for filter papers by year

--- spec/features/papers/index.html.erb_spec.rb
+++ spec/features/papers/index.html.erb_spec.rb
@@ -1,25 +1,22 @@
 require 'rails_helper'
 
-RSpec.describe "papers/index", type: :view do
+RSpec.describe "papers/index", type: :feature do
   before(:each) do
-    assign(:papers, [
-      Paper.create!(
-        title: "Title",
-        venue: "Venue",
-        year: 2
-      ),
-      Paper.create!(
-        title: "Title",
-        venue: "Venue",
-        year: 2
-      )
-    ])
+    @paper1 = build :paper
+    @paper1.year = 1950
+    @paper1.save
+    @paper2 = build :paper
+    @paper2.year = 2000
+    @paper2.title = "Test"
+    @paper2.save
   end
 
   it "renders a list of papers" do
-    render
-    assert_select "tr>td", text: "Title".to_s, count: 2
-    assert_select "tr>td", text: "Venue".to_s, count: 2
-    assert_select "tr>td", text: 2.to_s, count: 2
+    visit papers_path
+  end
+
+  it "filters papers by year" do
+    visit papers_path(year: 2000)
+    expect(page).not_to have_text(@paper1.title)
   end
 end
